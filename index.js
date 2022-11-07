2428317e54a928a871cf7f5d5c3a07914458f7f8
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2022-11-08 05:30:37 +0600

    environment-variable and database-connected added

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
+require('dotenv').config();
+
+
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.trx5yvh.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Wildlife Photography is running')
